5121a80503e3ad03ee7f54bc5119d4f75643d8d1
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-30 12:38:20 -0800

    Sample.c
    A sample C program.

---
+++ FINF/Database/DB/File/(.)/c/Samples/Sample.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+// A sample C program
+// Hello World in C
+
+int main(void)
+{
+    printf("hello, world\n");
+}
+// End of script
+// Lasted edited on 2021 Tuesday November 30th at 12:38 pm
