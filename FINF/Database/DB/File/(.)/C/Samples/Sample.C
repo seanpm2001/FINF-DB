d3203f5300344ecf82960809142e29a6ad831dc5
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-30 12:43:01 -0800

    Sample.C (V1)
    Version 1 of the sample C++ program.

---
+++ FINF/Database/DB/File/(.)/C/Samples/Sample.C
@@ -0,0 +1,8 @@
+#include <iostream>
+// A hello world program in C++
+int main()
+{
+    std::cout << "Hello, world!\n";
+}
+// A sample C++ program
+// Last edited on 2021 Tuesday November 30th at 12:43 pm
