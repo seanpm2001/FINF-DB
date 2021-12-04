61c3d9e15e7470122d58cd1231b3d53963b42940
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-04 14:30:20 -0800

    HLSL_SAMPLE1_V1.hlsl
    An archive of the first version of the sample HLSL file for this directory.

---
+++ FINF/Database/DB/File/(.)/h/l/s/l/Samples/OldVersions/1/1-100/HLSL_SAMPLE1_V1.hlsl
@@ -0,0 +1,4 @@
+// Please do not report this file as not working, as it does not work. I only created it, so it would be given an icon. A better example will be added in the future.
+// Last edited on 2021 Friday December 3rd at 7:03 pm, created 1 day earlier than published
+// Line count (including blank lines and compiler line): 5
+// File version: 1
