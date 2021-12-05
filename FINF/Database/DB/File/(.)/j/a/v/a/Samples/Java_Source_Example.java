7175fc2ab5859ce1f7c4adddf761e8e1154b68f0
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-04 17:21:03 -0800

    Java_Source_Example.java (V1)
    Version 1 of the sample Java file for this project.

---
+++ FINF/Database/DB/File/(.)/j/a/v/a/Samples/Java_Source_Example.java
@@ -0,0 +1,13 @@
+// Start of script
+// A sample Java Hello World program
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Prints the string to the console.
+        break;
+}}
+/* File info
+* File type: Java source file (*.java)
+* File version: 1 (2021, Saturday, December 4th at 5:21 pm)
+* Line count (including blank lines and compiler line): 14
+*/
+// End of script
