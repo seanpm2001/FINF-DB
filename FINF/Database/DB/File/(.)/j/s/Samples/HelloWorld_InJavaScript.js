2812229dc8ee8fec6b07baa9ec7cc7f2cbdbde0f
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-04 17:33:31 -0800

    HelloWorld_InJavaScript.js (V1)
    Version 1 of a Hello World program in JavaScript.

---
+++ FINF/Database/DB/File/(.)/j/s/Samples/HelloWorld_InJavaScript.js
@@ -0,0 +1,13 @@
+// Start of script
+class HelloWorld() {
+document.write("Hello World!");
+alert("Hello World!");
+console.log("Hello World");
+break;
+}
+/* File info
+* File type: JavaScript source file (*.js)
+* File version: 1 (2021, Saturday, December 4th at 5:33 pm)
+* Line count (including blank lines and compiler line): 14
+*/
+// End of script
